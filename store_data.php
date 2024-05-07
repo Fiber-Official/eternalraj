6b29b7c329c52a71a35cee9e3a589c3ae403bc1d
Fiber-Official <124904309+Fiber-Official@users.noreply.github.com>
2024-05-07 21:19:20 +0530

    Update store_data.php

--- store_data.php
+++ store_data.php
@@ -8,4 +8,5 @@ $location = $data['location']; // Replace with accurate location information
 $file = fopen("data.txt", "a");
 fwrite($file, "Name: $name, IP: $ip, Location: $location\n");
 fclose($file);
+echo "success";
 ?>
