033204225cb11c5e17fe4c131b0c2bfca9e3a456
Fiber-Official <124904309+Fiber-Official@users.noreply.github.com>
2024-05-07 21:27:59 +0530

    Update store_data.php

--- store_data.php
+++ store_data.php
@@ -8,5 +8,9 @@ $location = $data['location']; // Replace with accurate location information
 $file = fopen("data.txt", "a");
 fwrite($file, "Name: $name, IP: $ip, Location: $location\n");
 fclose($file);
-echo "success";
+if ($name === 'Raj') {
+    echo "success"; 
+} else {
+    echo "failure"; // Or some other signal to indicate the condition failed
+}
 ?>
